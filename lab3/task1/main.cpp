a817b9d75061dea86c8a7db673a5042ea3d11580
breakneckz <147052009+breakneckz@users.noreply.github.com>
2023-11-13 10:46:45 +0200

    Create main.cpp

---
+++ lab3/task1/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int hello (int a)
+{
+    int res = a + a;
+    return res; 
+}
+
+int main()
+{
+    int result = hello(5);
+
+     std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
